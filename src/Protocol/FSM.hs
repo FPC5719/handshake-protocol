c6c4664d1060b6f4d6bb0f871dc84b4b151c5840
fpc5719 <fpc5719@163.com>
2025-02-25 21:37:28 +0800

    Refactor. Should work.

--- src/Protocol/FSM.hs
+++ src/Protocol/FSM.hs
@@ -23,28 +23,10 @@ Motivations:
 module Protocol.FSM where
 
 import Clash.Prelude
-import Data.Either
 import Data.Profunctor
-{-
--- | Type with an initial value. Used for implicit states.
-class Initial a where
-  initial :: a
 
-instance Initial () where
-  initial = ()
 
-instance Initial Bool where
-  initial = False
 
-instance (Initial a, Initial b) => Initial (Either a b) where
-  initial = Left initial
-
-instance (Initial a, Initial b) => Initial (a, b) where
-  initial = (initial, initial)
-
-instance Initial a => Initial (Maybe a) where
-  initial = Just initial
--}
 -- | @`FSM` r i o s u v@: Finite State Machine.
 --
 -- Type parameters:
@@ -53,17 +35,19 @@ instance Initial a => Initial (Maybe a) where
 --     [@i@]: Input.
 --     [@o@]: Output.
 --     [@s@]: Auto generated state.
---     [@u@]: Data from the last FSM.
---     [@v@]: Data to the next FSM.
+--     [@u@]: Data from the last @`FSM`@.
+--     [@v@]: Data to the next @`FSM`@.
 --
 -- @`FSM`@ is indeed a wrapper around the function type
--- @`Maybe` (i, r, s, `Maybe` u) -> (o, r, `Either` s
--- (`Maybe` v))@. The input @`Maybe`@ determines whether
--- the @`FSM`@ is running or not. @`Either`@ in the output
--- controls state transition, in which @`Left` s@ means
--- updating the state of the current @`FSM`@, and
--- @`Right` v@ means terminating the current @`FSM`@,
--- sending data @v@ to the next @`FSM`@.
+-- @i -> r -> `Either` s u -> `Either` (o, r, s) v@.
+--
+-- The argument @`Either` s u@ indicates whether the
+-- @`FSM`@ is newly invoked with data @u@, or is already
+-- working on state @s@.
+--
+-- The result, @`Either` (o, r, s) v@, indicates whether
+-- the @`FSM`@ terminates with data @v@, or transfers to
+-- a new state @(r, s)@ with output @o@.
 data FSM r i o s u v
   = FSM
     { unFSM
@@ -71,21 +55,21 @@ data FSM r i o s u v
       -> Either (o, r, s) v
     }
 
-{-
+
 -- | @`Profunctor`@ instance for @u@ @v@. __Note__:
 -- For @i@ @o@ it is also a Profunctor, but less
 -- frequently used.
 instance IsFSM r i o s => Profunctor (FSM r i o s) where
-  dimap fl fr (FSM f) = fsm $ \(i, r, s, mu) ->
-    (fmap . fmap $ fr) . f $ Just (i, r, s, fl <$> mu)
--}
+  dimap fl fr (FSM f) = FSM $ \i r esu ->
+    fr <$> f i r (fl <$> esu)
+
 
 -- | Constraint type for @`FSM`@.
 type IsFSM :: Type -> Type -> Type -> Type -> Constraint
 type IsFSM r i o s =
   ( NFDataX r, Monoid r
   , Monoid o
-  , NFDataX s -- , Initial s
+  , NFDataX s
   )
 
 -- | Wrap @`FSM`@ with existential type so that @s@ can be
@@ -105,7 +89,7 @@ mealyFSM' (FSM' (FSM f)) = mealy go (mempty, Nothing)
     go (r, ms) i =
       case f i r $ maybe (Right ()) Left ms of
         Left (o, r', s') -> ((r', Just s'), o)
-        Right _ -> ((mempty, Nothing), mempty)
+        Right _ -> ((r, Nothing), mempty)
 
 -- | Embed a state transition into a @`FSM`@, which takes
 -- one cycle to perform.
@@ -175,12 +159,14 @@ FSM f &| FSM g = FSM $ \i r esu ->
       p (Just (Left (o, r', s))) = Just (o, r', s)
       p (Just (Right ())) = Nothing
   in case (p (f i r <$> fi), p (g i r <$> gi)) of
+    -- Only finish when both are finished
     (Nothing, Nothing) -> Right ()
+    -- Otherwise compose the result
     (Just (fo, fr, s1'), Nothing) -> Left (fo, fr, (Just s1', Nothing))
     (Nothing, Just (go, gr, s2')) -> Left (go, gr, (Nothing, Just s2'))
     (Just (fo, fr, s1'), Just (go, gr, s2')) ->
       Left (fo `mappend` go, fr `mappend` gr, (Just s1', Just s2'))
-{-
+
 -- | Combine two @`FSM`@s in parallel (Ensures at least one is finished).
 --
 --  __Note__: See also @`(&|)`@ for comparison.
@@ -193,67 +179,57 @@ FSM f &| FSM g = FSM $ \i r esu ->
   -> FSM r i o (s1, s2) (u1, u2) ()
   -- ^  No data is sent to the next @`FSM`@ (@v ~ ()@),
   -- to avoid unintended registers.
-FSM f &! FSM g = fsm $ \(i, r, (s1, s2), mu12) ->
-  let fe = f $ Just (i, r, s1, fst <$> mu12)
-      ge = g $ Just (i, r, s2, snd <$> mu12)
-  in case (fe, ge) of
-    (Left (fo, fr, s'), Left (go, gr, t')) ->
-      Left (fo `mappend` go, fr `mappend` gr, (s', t'))
-    _ -> Right (Just ())
-{-
+FSM f &! FSM g = FSM $ \i r esu ->
+  let (fi, gi) = case esu of
+        Left (s1, s2)  -> (Left s1, Left s2)
+        Right (u1, u2) -> (Right u1, Right u2)
+  in case (f i r fi, g i r gi) of
+    (Left (fo, fr, s1'), Left (go, gr, s2')) ->
+      Left (fo `mappend` go, fr `mappend` gr, (s1', s2'))
+    -- Finish once either is finished
+    _ -> Right ()
+
 -- | Conditionally combine two @`FSM`@s, invoking one branch
 -- based on an @`Either`@ value.
 (&+)
   :: ( IsFSM r i o s1
      , IsFSM r i o s2
      )
-  => FSM r i o s1 u1 v
-  -> FSM r i o s2 u2 v
-  -> FSM r i o (Either () (Either s1 s2)) (Either u1 u2) v
-  -- ^ Cannot replace @`Either` () a@ with @`Maybe` a@, because
-  -- of the difference in @`Initial`@ instance.
-FSM f &+ FSM g = fsm $ \(i, r, ees, meu) ->
-  let (fo, fr, fesv) = f $ case (ees, meu) of
-        -- Init and select Left.
-        (Left (), Just (Left u1))  -> Just (i, r, initial, Just u1)
-        -- Continuing with Left.
-        (Right (Left s1), _)       -> Just (i, r, s1, Nothing)
-        _                          -> Nothing
-      (go, gr, gesv) = g $ case (ees, meu) of
-        -- Init and select Right.
-        (Left (), Just (Right u2)) -> Just (i, r, initial, Just u2)
-        -- Continuing with Right.
-        (Right (Right s2), _)      -> Just (i, r, s2, Nothing)
-        _                          -> Nothing
-      -- Calculate the next state.
-      nst = case (ees, meu) of
-        -- Leave the initial state.
-        (Left (), Just (Left _))  -> Just (Left  ())
-        (Right (Left _), _)       -> Just (Left  ())
-        (Left (), Just (Right _)) -> Just (Right ())
-        (Right (Right _), _)      -> Just (Right ())
-        -- Stay at the initial state.
-        (Left (), Nothing)        -> Nothing
-  in case nst of
-    Nothing -> (mempty, mempty, Left (Left ()))
-    Just (Left  ()) -> (fo, fr,) $ case fesv of
-      Left s' -> Left (Right (Left s'))
+  => FSM r i o s1 u1 v -- ^ Branch on @`Left`@.
+  -> FSM r i o s2 u2 v -- ^ Branch on @`Right`@.
+  -> FSM r i o (Either s1 s2) (Either u1 u2) v
+FSM f &+ FSM g = FSM $ \i r esu ->
+  let inp = case esu of
+        -- Determine which branch to choose
+        Left  (Left s1)  -> Left  (Left  s1)
+        Right (Left u1)  -> Left  (Right u1)
+        Left  (Right s2) -> Right (Left  s2)
+        Right (Right u2) -> Right (Right u2)
+  in case inp of
+    Left fi -> case f i r fi of
+      Left (o, r', s1') -> Left (o, r', Left s1')
       Right v -> Right v
-    Just (Right ()) -> (go, gr,) $ case gesv of
-      Left s' -> Left (Right (Right s'))
+    Right gi -> case g i r gi of
+      Left (o, r', s2') -> Left (o, r', Right s2')
       Right v -> Right v
--}
+
 -- | Loop until the predicate is satisfied.
 -- The @`FSM`@ would be invoked for at least once.
 loop
   :: IsFSM r i o s
-  => (Maybe u -> Bool) -- ^ Predicate.
+  => (u -> Bool) -- ^ Predicate.
   -> FSM r i o s () u
-  -> FSM r i o s () u
-loop p (FSM f) = fsm $ \(i, r, s, mx) ->
-  case f $ Just (i, r, s, mx) of
-    Left (o, r', s') -> Left (o, r', s')
-    Right mu -> if p mu
-      then Right mu
-      else Left (mempty, mempty, initial)
--}
+  -> FSM r i o (Maybe s) () u -- ^ Add one more state.
+loop p (FSM f) = FSM $ \i r ems ->
+  let fi = case ems of
+        -- Restart from previous loop
+        Left Nothing  -> Right ()
+        -- Continue running
+        Left (Just s) -> Left s
+        -- New start from last @FSM@
+        Right ()      -> Right ()
+  in case f i r fi of
+    Left (o, r', s') -> Left (o, r', Just s')
+    Right u -> if p u
+      then Right u -- Predicate is satisfied
+      else Left (mempty, r, Nothing) -- Restart
